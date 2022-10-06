533f58037400a3f572e2c40a63599d57efd1e258
christianbrusa <christianbrusa@hotmail.com>
2022-10-06 00:29:53 -0300

    Importo router en index.js

--- desafioApiRest/server/index.js
+++ desafioApiRest/server/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const router = require("./init/routes");
 
 const app = express();
 const port = 8080;
+
+app.use("/", router);
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
