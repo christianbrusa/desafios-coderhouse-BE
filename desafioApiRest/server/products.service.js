7aa70ce87565ebbe1183753ce294636287e798ef
christianbrusa <christianbrusa@hotmail.com>
2022-10-06 00:27:07 -0300

    Agrego service con los metodos solicitados

---
+++ desafioApiRest/server/products.service.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+class Container {
+
+    constructor(name_file){
+        this.name_file = name_file;
+    }
+
+    async readFile(){
+        let data = await fs.promises.readFile(`./db/${this.name_file}`, "utf-8");
+        return JSON.parse(data);
+    }
+
+    async listAllProducts(){
+        const data = await this.readFile();
+        return data;
+    }
+
+    async listRandomProduct(){
+        const data = await this.readFile();
+        let random = Math.floor(Math.random()*data.length);
+        return data[random];
+    }
+    
+}
+
+module.exports = Container;
