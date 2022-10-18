508dd14150dc579812879b77409aa1b357e2d815
christianbrusa <christianbrusa@hotmail.com>
2022-10-17 23:37:42 -0300

    Agrego config handlebars

--- desafioApiRest/server/index.js
+++ desafioApiRest/server/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const hbs = require("express-handlebars");
+const path = require('path');
 const router = require("./init/routes");
 
 const app = express();
@@ -6,6 +8,13 @@ const port = 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
+
+const viewsPath = path.join(__dirname, '../views/layouts/');
+app.engine("hbs", hbs.engine({defaultLayout: false, extname: "hbs"}));
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+app.use(express.static("public"));
+
 app.use("/api/productos", router);
 
 app.listen(port, () => {
